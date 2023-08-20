a5045e0d0b60f8bace776fa5272e824fcaec9367
danyreis <dany.reis@hotmail.com>
2023-08-20 16:35:46 -0300

    CódigoMedia

---
+++ src/Proj1.java
@@ -0,0 +1,11 @@
+public class Proj1 {
+    public static void main(String[] args) {
+        float media = 6;
+            if (media >= 6.0)
+                System.out.println("Aluno Aprovado");
+            else {
+                System.out.println("Aluno Reprovado");
+            }
+
+    }
+}
